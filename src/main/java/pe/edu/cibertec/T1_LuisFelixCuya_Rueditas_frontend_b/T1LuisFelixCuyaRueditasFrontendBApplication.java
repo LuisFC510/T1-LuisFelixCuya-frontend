b78cc4eb19e40134c193872e42fb62bcf58ca833
Luis Felix Cuya <luisfelixcuya10@gmail.com>
2024-09-22 22:50:19 -0500

    Cargar inicial del proyecto

---
+++ src/main/java/pe/edu/cibertec/T1_LuisFelixCuya_Rueditas_frontend_b/T1LuisFelixCuyaRueditasFrontendBApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.T1_LuisFelixCuya_Rueditas_frontend_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class gitT1LuisFelixCuyaRueditasFrontendBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1LuisFelixCuyaRueditasFrontendBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/T1_LuisFelixCuya_Rueditas_frontend_b/T1LuisFelixCuyaRueditasFrontendBApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.T1_LuisFelixCuya_Rueditas_frontend_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1LuisFelixCuyaRueditasFrontendBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
